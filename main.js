553936c4b3a7f05cf4ff27987ddd4f9e67e9c8b7
Aditya-Lamba <78349391+Aditya-Lamba@users.noreply.github.com>
2021-02-01 15:07:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function oncliclck="GreatWall()"{
+    document.getElementById("audio").play();
+}
+
+function onclick="Colosseum()" {
+    document.getElementById("audio").play();
+}
+
+function onclick="TajMahal()" {
+    document.getElementById("audio").play();
+}
+
+function onclick="MachuPicchu()" {
+    document.getElementById("audio").play();
+}
+
+function onclick="StatueInBrazil()" {
+    document.getElementById("audio").play();
+}
+
+function onclick="ChichenItza()" {
+    document.getElementById("audio").play();
+}
+
+function onclick="petra()" {
+    document.getElementById("audio").play();
+}
